a90786671418bd3f620ce9c76861b175a60b2f24
Chris Malley <cmalley@pixelzoom.com>
2013-10-23 13:16:17 -0600

    joist#59: replace duck-typing with instantiation or subtyping of joist.Screen, so that we can add 'navbar icon' functionality to Screen.

--- js/blast-main.js
+++ js/blast-main.js
@@ -1,17 +1,16 @@
 // Copyright 2002-2013, University of Colorado Boulder
 
-require( [ 'JOIST/SimLauncher', 'JOIST/Sim', 'SCENERY/nodes/Rectangle', 'model/BlastModel', 'view/BlastView'],
-  function( SimLauncher, Sim, Rectangle, BlastModel, BlastView ) {
+require( [ 'JOIST/SimLauncher', 'JOIST/Sim', 'JOIST/Screen', 'SCENERY/nodes/Rectangle', 'model/BlastModel', 'view/BlastView'],
+  function( SimLauncher, Sim, Screen, Rectangle, BlastModel, BlastView ) {
     'use strict';
 
     SimLauncher.launch( function() {
       new Sim( 'Blast', [
-        {
-          name: 'Blast',
-          icon: new Rectangle( 0, 0, 10, 10, {fill: 'red'} ),
-          createModel: function() {return new BlastModel();},
-          createView: function( model ) {return new BlastView( model );}
-        }
+        new Screen( 'Blast',
+          new Rectangle( 0, 0, 10, 10, {fill: 'red'} ),
+          function() {return new BlastModel();},
+          function( model ) {return new BlastView( model );}
+        )
       ] ).start();
     } );
   } );
