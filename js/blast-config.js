fd095c92db44983f371716549069aafe0246219b
Jonathan Olson <olsonsjc@gmail.com>
2014-11-12 00:01:45 -0700

    Changing assertions to the preloaded assert/js/assert.js (which should be included in any HTML file that will include JS running our assertions). see https://github.com/phetsims/scenery/issues/300

--- js/blast-config.js
+++ js/blast-config.js
@@ -14,7 +14,6 @@ require.config( {
     text: '../../sherpa/text',
 
     // PhET libs, uppercase names to identify them in require.js imports
-    ASSERT: '../../assert/js',
     AXON: '../../axon/js',
     DOT: '../../dot/js',
     JOIST: '../../joist/js',
