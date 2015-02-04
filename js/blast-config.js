ec3b86aa3c6e815cf17c44bfe29d7112cf33dca7
Sam Reid <reids@colorado.edu>
2015-02-03 20:52:45 -0700

    Added cache buster as an option & standardized formatting for config files, see https://github.com/phetsims/joist/issues/196

--- js/blast-config.js
+++ js/blast-config.js
@@ -29,5 +29,6 @@ require.config( {
     BLAST: '.'
   },
 
-  urlArgs: new Date().getTime()  // cache buster to make browser refresh load all included scripts
+  // optional cache buster to make browser refresh load all included scripts, can be disabled with ?cacheBuster=false
+  urlArgs: phet.phetcommon.getCacheBusterArgs()
 } );
