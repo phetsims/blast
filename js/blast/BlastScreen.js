6cada71bf3d91c8c6a316d0ca7c2619c089f47e8
samreid <reids@colorado.edu>
2020-05-28 17:05:01 -0600

    Use default icons, see https://github.com/phetsims/joist/issues/626

--- js/blast/BlastScreen.js
+++ js/blast/BlastScreen.js
@@ -9,7 +9,6 @@
 import Screen from '../../../joist/js/Screen.js';
 import inherit from '../../../phet-core/js/inherit.js';
 import merge from '../../../phet-core/js/merge.js';
-import Rectangle from '../../../scenery/js/nodes/Rectangle.js';
 import blast from '../blast.js';
 import BlastModel from './model/BlastModel.js';
 import BlastScreenView from './view/BlastScreenView.js';
@@ -25,11 +24,6 @@ function BlastScreen( tandem, options ) {
     particleColor: 'black',
     tandem: tandem
   }, options );
-
-  assert && assert( !options.homeScreenIcon );
-  options.homeScreenIcon = Rectangle.rect( 0, 0, Screen.MINIMUM_HOME_SCREEN_ICON_SIZE.width, Screen.MINIMUM_HOME_SCREEN_ICON_SIZE.height, {
-    fill: options.backgroundColor
-  } );
 
   Screen.call( this,
     function() { return new BlastModel( tandem.createTandem( 'model' ) ); },
